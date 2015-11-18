1fc7f93aa1a153f83b2d443a0f839af8c8b0fe6f
ccfz <casparfz@gmail.com>
2015-11-18 16:13:46 +0100

    blackjack done

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_sum = deal_card+deal_card
+  display_card_total(card_sum)
+  card_sum
 end
 
-def hit?
-  # code hit? here
+def hit?(number)
+  prompt_user
+  input = get_user_input
+  if input == "s"
+    number
+    #binding.pry
+  elsif input == "h"
+    deal_card+number
+    #binding.pry 
+  else
+    invalid_command
+  end
+
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
@@ -39,6 +55,16 @@ end
 #####################################################
 
 def runner
-  # code runner here
+welcome
+current_sum=hit?(initial_round)
+display_card_total(current_sum)
+  until current_sum>=21  do
+  current_sum=hit?(current_sum)
+  end
+  end_game(current_sum)
+  
+
 end
+
+
     
